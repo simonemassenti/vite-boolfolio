635239f2ae1f75e2af273ba3cae49186874b795e
simonemassenti <s.massenti96@gmail.com>
2024-02-01 15:17:09 +0100

    add: portfolio log and cards

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <script>
 import axios from 'axios';
+import PortfolioCard from './components/PortfolioCard.vue';
 
 export default{
     data() {
         return {
             base_url: "http://127.0.0.1:8000",
             portfolios: []
-        }
+        };
     },
     created() {
         axios
-        .get(`${this.base_url}/api/portfolios`)
-        .then((resp) => {
+            .get(`${this.base_url}/api/portfolios`)
+            .then((resp) => {
             console.log(resp);
-        })
-    }
+            this.portfolios = resp.data.results;
+        });
+    },
+    components: { PortfolioCard }
 }
 
 </script>
 
 <template>
-    <h1>All Portfolios</h1> 
-
+    <h1 class="text-center mt-3">All Portfolios</h1> 
+    
+    <PortfolioCard :portfolios="portfolios"/>
     
 </template>
 
---
+++ src/components/PortfolioCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    props: {
+        portfolios: Object
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container my-4">
+        <div class="row row-cols-4 g-3">
+            <div v-for="portfolio in portfolios" class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ portfolio.title }}</h5>
+                        <p class="card-text">{{ portfolio.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
