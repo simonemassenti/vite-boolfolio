e5d1de6157055a15447be980db8f1ce057202b39
simonemassenti <s.massenti96@gmail.com>
2024-02-01 14:52:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export default{
+    data() {
+        return {
+            base_url: "http://127.0.0.1:8000",
+            portfolios: []
+        }
+    },
+    created() {
+        axios
+        .get(`${this.base_url}/api/portfolios`)
+        .then((resp) => {
+            console.log(resp);
+        })
+    }
+}
+
+</script>
+
+<template>
+    <h1>All Portfolios</h1> 
+
+    
+</template>
+
+<style lang="scss">
+@use "./assets/styles/general.scss" as *;
+
+</style>
